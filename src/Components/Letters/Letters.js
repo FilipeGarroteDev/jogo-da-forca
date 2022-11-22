001aec93cd3004d1b0d8074165d58d4b8fec4343
Filipe Garrote <filipe.garrote@gmail.com>
2022-11-22 14:31:46 -0300

    feat: Criação do componente 'Letters' com renderização dinâmica a partir da array do alfabeto

---
+++ src/Components/Letters/Letters.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { alphabet } from "../../Common/database";
+
+export default function Letters() {
+	return (
+		<Keyboard>
+			{alphabet.map((letter) => (
+				<LetterBox>{letter}</LetterBox>
+			))}
+		</Keyboard>
+	);
+}
+
+const Keyboard = styled.section`
+  max-width: 664px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  margin: auto auto;
+
+`
+
+const LetterBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+	width: 40px;
+	height: 40px;
+	border: 1px solid #7aa7c7;
+  border-radius: 3px;
+	background-color: #e1ecf4;
+	color: #7aa7c7;
+	font-size: 16px;
+	font-weight: 700;
+`;
